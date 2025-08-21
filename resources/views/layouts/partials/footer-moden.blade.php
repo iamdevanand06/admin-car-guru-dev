c103a79f4aa1608640ba6ad57df08bfe484cee8b
iamdevanand06 <devanand338@gmail.com>
2025-08-21 13:43:56 +0530

    Footer Implementation

---
+++ resources/views/layouts/partials/footer-moden.blade.php
@@ -0,0 +1,5 @@
+<div class="footer d-sm-flex align-items-center justify-content-between border-top bg-white p-3">
+    <p class="mb-0 text-gray-9">2014 - @php echo date("Y"); @endphp &copy; MacroMend Technologies. All Right Reserved
+    </p>
+    <p>Designed &amp; Developed by <a href="javascript:void(0);" class="text-primary">MacroMend Technologies</a></p>
+</div>
--- resources/views/mmv/makes/index.blade.php
+++ resources/views/mmv/makes/index.blade.php
@@ -106,9 +106,6 @@
             </div>
             <!-- /product list -->
         </div>
-        <div class="footer d-sm-flex align-items-center justify-content-between border-top bg-white p-3">
-            <p class="mb-0 text-gray-9">2014 - 2025 &copy; DreamsPOS. All Right Reserved</p>
-            <p>Designed &amp; Developed by <a href="javascript:void(0);" class="text-primary">Dreams</a></p>
-        </div>
+        @include('layouts.partials.footer-moden')
     </div>
 @endsection
